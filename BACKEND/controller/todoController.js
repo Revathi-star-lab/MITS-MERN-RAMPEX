557ffa97a94e5d84107e3091341f8140cf35907c
Revathi Pilli <revathipilli421@gmail.com>
2026-02-05 10:07:05 +0530

    BACKEND Api created

--- BACKEND/controller/todoController.js
+++ BACKEND/controller/todoController.js
@@ -1,34 +1,103 @@
-let todos=[];
+let todos=[]
 let id=1;
-
-exports.getTodo=(req,res)=>{
-    res.status(200).json(todos)
-};
-exports.createTodo=(req,res)=>{
-     const {task}=req.body;
-     const newTodo={
-          id:id++,
-          task,
-          Completd:false
-     }
-     todos.push(newTodo)
-     res.json(newTodo)
-};
-exports.updateTodo=(req,res)=>{
-    const todo=todos.find((t)=>t.id===parseInt(req.params.id));
-    if(!todo){
-     res.json({message:"Todo not found!"})
+const Todo=require("../models/todo");
+exports.getTodo=async(req,res)=>{
+    try{
+         const todos=await Todo.find();
+         res.status(200).json(todos);
+    }catch(err)
+    {
+        res.status(500).send(err);
     }
-    todo.task= req.body.task || todo.task;
-    todo.Completed=req.body.Completed===undefined?todo.Completed:req.body.Completed
-    res.json(todo)
-//     console.log(todo)
+    
+
+    // res.status(200).json(todos)
+    // res.send("Get Working")
 };
-exports.deleteTodo=(req,res)=>{
-     //res.send("Delete working")
-     const index=todos.findIndex(t=>t.id===parseInt(req.params.id))
-     if(index===-1)
-          return res.status(404).json({message:"Task not found"})
-     todos.filter((_,i)=>i!==index);
-     res.status(200).json({message:"Task deleted successfully"})
+
+exports.createTodo=async(req,res)=>{
+    try{
+        const {task} =req.body;
+       if (task===undefined)
+       {
+        return res.status(401).json({message:"Task not found"});
+       }
+       const todos=await Todo.create({
+        task,
+        completed:false
+       })
+       res.status(201).json(todos);
+    }catch(err){
+       res.status(500).send(err);
+    }
+
+    };
+
+    // res.send("Post working")
+exports.updateTodo=async(req,res)=> {
+   try{
+    //  const todo= await Todo.findById(req.params.id);
+    // if(!todo){
+    //     res.status(404).json({message:"Todo not found"})
+    // }
+    // todo.task=req.body.task || todo.task;
+    // todo.completed=req.body.completed===undefined?todo.completed:req.body.completed
+    // await todo.save();
+    const todo=await Todo.findByIdAndUpdate(req.paramas.id,req.body,{new:true})
+    res.status(200).json(todo)
+   }catch(err){
+    res.status(500).send(err);
+
+   }
+    // console.log(todo)
+    // const todo=todos.find(t=>(t.id===index)===parseInt(req.body))
+    // res.send("Put working")
 };
+exports.deleteTodo=async(req,res)=>{
+    try{
+        await Todo.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:"Task deleted successfully"})
+    }catch(err)
+    {
+        res.status(500).send(err);
+    }
+    
+}
+
+
+
+
+// let todos=[];
+// let id=1;
+
+// exports.getTodo=(req,res)=>{
+//     res.status(200).json(todos)
+// };
+// exports.createTodo=(req,res)=>{
+//      const {task}=req.body;
+//      const newTodo={
+//           id:id++,
+//           task,
+//           Completd:false
+//      }
+//      todos.push(newTodo)
+//      res.json(newTodo)
+// };
+// exports.updateTodo=(req,res)=>{
+//     const todo=todos.find((t)=>t.id===parseInt(req.params.id));
+//     if(!todo){
+//      res.json({message:"Todo not found!"})
+//     }
+//     todo.task= req.body.task || todo.task;
+//     todo.Completed=req.body.Completed===undefined?todo.Completed:req.body.Completed
+//     res.json(todo)
+// //     console.log(todo)
+// };
+// exports.deleteTodo=(req,res)=>{
+//      //res.send("Delete working")
+//      const index=todos.findIndex(t=>t.id===parseInt(req.params.id))
+//      if(index===-1)
+//           return res.status(404).json({message:"Task not found"})
+//      todos.filter((_,i)=>i!==index);
+//      res.status(200).json({message:"Task deleted successfully"})
+// };
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,14 @@
+
 const express = require("express")
 const todoRoute=require("./router/todoRouter")
 const dotenv=require("dotenv");
-const userRoute=require("./router/userRouter");
+const userRoute = require("./router/userRouter");
+const connectDB=require("./config/db")
+const cors=require("cors")
 dotenv.config();
+connectDB();
 const app=express();
+app.use(cors());
 app.use(express.json())
 app.use("/api/todo",todoRoute);
 app.use("/api/users",userRoute)
@@ -11,39 +16,58 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>
 {
     console.log(`Server running on port http:///localhost:${PORT}`)
-});
+})
 
 
 
-// const express=require("express");
-// const todoRoute=require("./router/todoRouter");
+// const express = require("express")
+// const todoRoute=require("./router/todoRouter")
 // const dotenv=require("dotenv");
-// const userRoute= require("./router/userRouter");
+// const userRoute=require("./router/userRouter");
+// const { db } = require("./models/todo");
 // dotenv.config();
 // const app=express();
+// connect db();
 // app.use(express.json())
 // app.use("/api/todo",todoRoute);
-// app.use("/api/users",userRoute);
-// const PORT=process.env.PORT||3000;
-// app.get("/",(req,res)=>{
-//     res.send("Get Route is Working")
-// })
-// app.post("/create",(req,res)=>
+// app.use("/api/users",userRoute)
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT,()=>
 // {
-//     res.json({
-//         message:"Post Route is working"
-//     })
-// })
-// app.put("/",(req,res)=>{
-//     res.status(200).json({
-//     message:"Put Route is working"
-//     })
-// })
-// app.delete("/",(req,res)=>{
-//     res.status(200).json({
-//         message:"Delete Route is working"
-//     })
-// })
-// app.listen(PORT,()=>{
-//     console.log(`server running on port http://localhost:${PORT}`)
-// })
+//     console.log(`Server running on port http:///localhost:${PORT}`)
+// });
+
+
+
+// // const express=require("express");
+// // const todoRoute=require("./router/todoRouter");
+// // const dotenv=require("dotenv");
+// // const userRoute= require("./router/userRouter");
+// // dotenv.config();
+// // const app=express();
+// // app.use(express.json())
+// // app.use("/api/todo",todoRoute);
+// // app.use("/api/users",userRoute);
+// // const PORT=process.env.PORT||3000;
+// // app.get("/",(req,res)=>{
+// //     res.send("Get Route is Working")
+// // })
+// // app.post("/create",(req,res)=>
+// // {
+// //     res.json({
+// //         message:"Post Route is working"
+// //     })
+// // })
+// // app.put("/",(req,res)=>{
+// //     res.status(200).json({
+// //     message:"Put Route is working"
+// //     })
+// // })
+// // app.delete("/",(req,res)=>{
+// //     res.status(200).json({
+// //         message:"Delete Route is working"
+// //     })
+// // })
+// // app.listen(PORT,()=>{
+// //     console.log(`server running on port http://localhost:${PORT}`)
+// // })
